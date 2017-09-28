e36775f1f8d9cf94df2b7bc1d9f6bc7d95388967
MayoufAppExperts <mmayouf@outlook.com>
2017-09-28 10:03:10 +0100

    connectionservice

--- app/src/main/java/com/example/theappexperts/flowerapi/services/ConnectionService.java
+++ app/src/main/java/com/example/theappexperts/flowerapi/services/ConnectionService.java
@@ -1,9 +1,13 @@
 package com.example.theappexperts.flowerapi.services;
 
 import com.example.theappexperts.flowerapi.API_Constants;
+import com.example.theappexperts.flowerapi.MyApp;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
+import java.io.File;
+
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -26,4 +30,24 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
             return retrofit.create(reqInterface.class);
         }
+
+    public static OkHttpClient buildClient() {
+//
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        //  builder.addNetworkInterceptor(new ResponseCacheInterceptor());
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        builder.addInterceptor(interceptor).build();
+        // Enable response caching
+        builder .addNetworkInterceptor(new Offline_CacheData.ResponseCacheInterceptor());
+        builder .addInterceptor(new Offline_CacheData.OfflineResponseCacheInterceptor())
+                // Set the cache location and size (5 MB)
+                .cache(new okhttp3.Cache(new File(MyApp
+                        .getContext().getCacheDir(),
+                        "apiResponses"), 5  1024  1024));
+        return new OkHttpClient();
+
+
+    }
 }
